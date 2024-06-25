dd4f5dc3247dd9afe8e8445b9fa989eded4e78d1
RasaIulian <ilie.rasa@gmail.com>
2024-06-25 10:48:04 +0300

    Updates

--- precache-manifest.d0e19a20c354c44ea73460342d1dbb8f.js
+++ precache-manifest.d0e19a20c354c44ea73460342d1dbb8f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "66fe5b21af63f2e4da5cd7de7cf0efd6",
+    "revision": "baa33a0fc4f001b26cacddbbc9d6294f",
     "url": "/WeatherApp/index.html"
   },
   {
-    "revision": "7c09cd5271ec5a123187",
+    "revision": "b81bae4548135a96529c",
     "url": "/WeatherApp/static/css/main.e239072b.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/WeatherApp/static/js/2.3476dd6f.chunk.js"
   },
   {
-    "revision": "7c09cd5271ec5a123187",
-    "url": "/WeatherApp/static/js/main.2a288be0.chunk.js"
+    "revision": "b81bae4548135a96529c",
+    "url": "/WeatherApp/static/js/main.2236dcc5.chunk.js"
   },
   {
     "revision": "4efbdecc212de1558e2a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/WeatherApp/precache-manifest.8e1780531c820a6b1451ed6f814ad5b6.js"
+  "/WeatherApp/precache-manifest.d0e19a20c354c44ea73460342d1dbb8f.js"
 );
 
 self.addEventListener('message', (event) => {
