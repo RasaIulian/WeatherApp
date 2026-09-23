f95c12616cc45fe4a95fa87696b07cc91b5886b8
Rasa Iulian <ilie.rasa@gmail.com>
2026-09-23 17:27:35 +0300

    altitude API updated to opentopodata and cloudflare server update to prevent CORS errors

--- src/hooks/getAltitude/getAltitude.jsx
+++ src/hooks/getAltitude/getAltitude.jsx
@@ -1,4 +1,3 @@
-// useAltitude.jsx - open-elevation.com
 import { useState } from "react";
 
 export const useAltitude = () => {
@@ -6,23 +5,34 @@ export const useAltitude = () => {
   const [altitudeError, setAltitudeError] = useState("");
 
   const fetchAltitude = async (latitude, longitude) => {
+    setLoadingAltitude(true);
+    setAltitudeError("");
+
     try {
-      setLoadingAltitude(true);
-      setAltitudeError("");
-      // Introduce an artificial delay to check loading state
-      // await new Promise((resolve) => setTimeout(resolve, 1000));
       const response = await fetch(
-        `https://api.open-elevation.com/api/v1/lookup?locations=${latitude},${longitude}`
+        `${process.env.REACT_APP_RAINVIEWER_PROXY}/v1/srtm90m?locations=${latitude},${longitude}`,
+        // api.opentopodata.org - see .env file
+        // srtm = Shuttle Radar Topography Mission
+        // 90m = resolution is about 90 meters per grid cell (good default choice)
+        // this is a global elevation dataset
       );
-      setLoadingAltitude(false);
+
       if (!response.ok) {
         throw new Error(`status ${response.status}`);
       }
+
       const data = await response.json();
+
+      if (data.status !== "OK") {
+        throw new Error(data.error || "unknown error");
+      }
+
       return data.results[0].elevation;
     } catch (error) {
       console.error("Error fetching altitude:", error.message);
       setAltitudeError("Error fetching altitude: " + error.message);
+      return undefined;
+    } finally {
       setLoadingAltitude(false);
     }
   };
